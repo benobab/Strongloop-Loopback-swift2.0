42369398f73b22747abb325934ae173b2c4a0072
Benjamin Lacroix <benlacroix83@gmail.com>
2015-08-01 21:48:05 +0100

    Initial Commit
    CoffeeShop working OK !!

---
+++ LoopBack-Example/CoffeeShop.swift
@@ -0,0 +1,21 @@
+//
+//  CoffeeShop.swift
+//  LoopBack-Example
+//
+//  Created by Benobab on 01/08/15.
+//  Copyright © 2015 com.red-cedar. All rights reserved.
+//
+
+import Foundation
+
+extension LBModel {
+    
+    struct CoffeeShop{
+        var name:String
+        
+        init(name:String){
+            self.name = name
+        }
+    }
+    
+}
